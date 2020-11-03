cab220a723673e3301a1cb92dcc1b5e1d5b9bdf1
Waflyano4ka <isip_v.s.veselov@mpt.ru>
2020-11-03 15:06:17 +0300

    New

---
+++ app/src/main/java/com/example/weatherapi/DataWeather.kt
@@ -0,0 +1,20 @@
+package com.example.weatherapi
+
+import android.os.Parcelable
+import kotlinx.android.parcel.Parcelize
+import org.json.JSONObject
+
+@Parcelize
+data class DataWeather (
+    var temp: Double,
+    var feels_like: Double,
+    var temp_min: Double,
+    var temp_max: Double,
+    var pressure: Int, // Давление
+    var humidity: Int, // Влажность
+    var visibility: Int,
+    var wind_speed: Int,
+    var wind_deg: Int,
+    var clouds: String,
+    var name: String
+): Parcelable
---
+++ app/src/main/java/com/example/weatherapi/MainActivity.kt
@@ -0,0 +1,25 @@
+package com.example.weatherapi
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.SearchView
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        Search.setOnQueryTextListener(object : SearchView.OnQueryTextListener{
+            override fun onQueryTextSubmit(query: String?): Boolean {
+                var request = GetWeatherData()
+                var dataWeather = request.GetRequest("http://api.openweathermap.org/data/2.5/weather?q=%D0%9C%D0%BE%D1%81%D0%BA%D0%B2%D0%B0&units=metric&appid=918c0ad6a6ed19ef0077f927868d3327")
+                ErrorString.text = dataWeather.name
+                return false
+            }
+            override fun onQueryTextChange(newText: String?): Boolean {
+                return false
+            }
+        })
+    }
+}
