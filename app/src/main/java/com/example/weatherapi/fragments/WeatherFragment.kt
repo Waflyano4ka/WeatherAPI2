3db51b932e7404fab49031d0732f7da98eb36b5c
Waflyano4ka <isip_v.s.veselov@mpt.ru>
2020-11-04 17:19:21 +0300

    RecyclerView

--- app/src/main/java/com/example/weatherapi/fragments/WeatherFragment.kt
+++ app/src/main/java/com/example/weatherapi/fragments/WeatherFragment.kt
@@ -9,6 +9,9 @@ import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.weatherapi.*
+import okhttp3.*
+import org.json.JSONObject
+import java.io.IOException
 
 
 class WeatherFragment : Fragment() {
@@ -26,13 +29,44 @@ class WeatherFragment : Fragment() {
 
         var weatherList: ArrayList<DataWeather> = ArrayList<DataWeather>()
         var getWeatherData : Weather = MainActivity()
-        weatherList = getWeatherData.GetWeatherData()
 
-        RecyclerView.adapter = ItemAdapter(context, weatherList){
-            val intent = Intent(context, DetailActivity::class.java)
-            intent.putExtra("OBJECT_INTENT", it)
-            startActivity(intent)
-        }
+        var request: Request = Request.Builder().url("https://api.openweathermap.org/data/2.5/weather?q=London&appid=918c0ad6a6ed19ef0077f927868d3327").build()
+        var okHttpClient: OkHttpClient = OkHttpClient()
+
+        var dataWeather = DataWeather(0.0,0.0,0.0,0.0,0,0,0,0,0,"","null")
+
+        okHttpClient.newCall(request).enqueue(object: Callback {
+            override fun onFailure(call: Call, e: IOException) {
+                dataWeather = DataWeather(0.0,0.0,0.0,0.0,0,0,0,0,0,"","Error")
+            }
+            override fun onResponse(call: Call, response: Response) {
+                val json = JSONObject(response!!.body!!.string())
+                var jsonMain = json.getJSONObject("main")
+                var temp: Double = jsonMain.getDouble("temp")
+                var feels_like: Double = jsonMain.getDouble("feels_like")
+                var temp_min: Double = jsonMain.getDouble("temp_min")
+                var temp_max: Double = jsonMain.getDouble("temp_max")
+                var pressure: Int = jsonMain.getInt("pressure")
+                var humidity: Int = jsonMain.getInt("humidity")
+
+                var visibility: Int = json.getInt("visibility")
+
+                var jsonWind = json.getJSONObject("wind")
+                var wind_speed: Int = jsonWind.getInt("speed")
+                var wind_deg: Int = jsonWind.getInt("deg")
+
+                var clouds: String = json.getJSONObject("clouds").toString()
+                var name: String = json.getString("name")
+
+                weatherList.add(DataWeather(temp, feels_like, temp_min, temp_max, pressure, humidity, visibility, wind_speed, wind_deg, clouds, name))
+
+                RecyclerView.adapter = ItemAdapter(context, weatherList){
+                    val intent = Intent(context, DetailActivity::class.java)
+                    intent.putExtra("OBJECT_INTENT", it)
+                    startActivity(intent)
+                }
+            }
+        })
         return view
     }
 }
