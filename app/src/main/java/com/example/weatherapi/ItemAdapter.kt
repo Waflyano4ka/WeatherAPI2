8d282830d14ef1d71cc7f3b980871397e24d3e1f
Waflyano4ka <isip_v.s.veselov@mpt.ru>
2020-11-06 17:09:21 +0300

    icon

--- app/src/main/java/com/example/weatherapi/ItemAdapter.kt
+++ app/src/main/java/com/example/weatherapi/ItemAdapter.kt
@@ -16,10 +16,32 @@ class ItemAdapter (
     class ImageViewHolder(view: View) : RecyclerView.ViewHolder(view){
         val titleScr = view.findViewById<TextView>(R.id.titleTv)
         val descriptionScr = view.findViewById<TextView>(R.id.descriptionTv)
+        val imageScr = view.findViewById<ImageView>(R.id.imageIv)
 
         fun bindView(image: DataWeather, listener: (DataWeather) -> Unit){
             titleScr.text = image.name
             descriptionScr.text = image.feels_like.toString()
+
+            when (image.weather_icon){
+                "01d" -> imageScr.setImageResource(R.drawable.w01d)
+                "02d" -> imageScr.setImageResource(R.drawable.w02d)
+                "03d" -> imageScr.setImageResource(R.drawable.w03d)
+                "04d" -> imageScr.setImageResource(R.drawable.w04d)
+                "09d" -> imageScr.setImageResource(R.drawable.w09d)
+                "10d" -> imageScr.setImageResource(R.drawable.w10d)
+                "11d" -> imageScr.setImageResource(R.drawable.w11d)
+                "13d" -> imageScr.setImageResource(R.drawable.w13d)
+                "50d" -> imageScr.setImageResource(R.drawable.w50d)
+                "01n" -> imageScr.setImageResource(R.drawable.w01n)
+                "02n" -> imageScr.setImageResource(R.drawable.w02n)
+                "03n" -> imageScr.setImageResource(R.drawable.w03n)
+                "04n" -> imageScr.setImageResource(R.drawable.w04n)
+                "09n" -> imageScr.setImageResource(R.drawable.w09n)
+                "10n" -> imageScr.setImageResource(R.drawable.w10n)
+                "11n" -> imageScr.setImageResource(R.drawable.w11n)
+                "13n" -> imageScr.setImageResource(R.drawable.w13n)
+                "50n" -> imageScr.setImageResource(R.drawable.w50n)
+            }
 
             itemView.setOnClickListener { listener(image) }
         }
--- app/src/main/java/com/example/weatherapi/fragments/AccurateWeatherFragment.kt
+++ app/src/main/java/com/example/weatherapi/fragments/AccurateWeatherFragment.kt
@@ -5,26 +5,155 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.animation.Animation
+import android.view.animation.AnimationUtils
 import com.example.weatherapi.*
 import com.google.gson.Gson
 import com.google.gson.reflect.TypeToken
 import kotlinx.android.synthetic.main.fragment_accurate_weather.view.*
 
+val phraze = listOf(
+    "This weather is made for swimming",
+    "Great weather today for a walk",
+    "Even though the sun is outside, it is better to dress warmly",
+    "Russians swim in such weather"
+)
+
 class AccurateWeatherFragment : Fragment() {
+    lateinit var mainView : View
     lateinit var weather: DataWeather
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        val mainView =  inflater.inflate(R.layout.fragment_accurate_weather, container, false)
+        mainView =  inflater.inflate(R.layout.fragment_accurate_weather, container, false)
         var gson = Gson()
         var json = arguments?.getString("weather")
         var type = object : TypeToken<DataWeather>() {}.type
 
         weather = gson.fromJson(json, type)
-        mainView.Text.text = weather.name
+        mainView.Name.text = weather.name
+        mainView.Degrees.text = weather.feels_like.toInt().toString() + "°"
+        mainView.MainFirst.text = weather.weather_main + "\n" + weather.temp_min.toInt() + " / " + weather.temp_max.toInt() + "°C"
+        mainView.Pressure.text = weather.pressure.toString() + " mb"
+        mainView.Humidity.text = weather.humidity.toString() + " %"
+        mainView.Visibility.text = weather.visibility.toString() + " m"
+        mainView.WindSpeed.text = weather.wind_speed.toString() + " m/s"
 
+        var sunTopAnim: Animation = AnimationUtils.loadAnimation(context, R.anim.clear_sky_day_animation)
+
+        var dayBackgroundColor = R.drawable.background_day_style
+
+        when (weather.weather_icon){
+            "01d" -> {
+                mainView.MainFrame.setBackgroundResource(dayBackgroundColor)
+                mainView.ImageSun.setImageResource(R.drawable.w01d)
+                mainView.ImageSun.animation = sunTopAnim
+                GetClearSkyPhrase(weather.temp.toInt())
+            }
+            "02d" -> {
+                mainView.MainFrame.setBackgroundResource(dayBackgroundColor)
+                mainView.ImageSun.setImageResource(R.drawable.w01d)
+                mainView.ImageSun.animation = sunTopAnim
+            }
+            "03d" -> {
+                mainView.MainFrame.setBackgroundResource(dayBackgroundColor)
+                mainView.ImageSun.setImageResource(R.drawable.w01d)
+                mainView.ImageSun.animation = sunTopAnim
+            }
+            "04d" -> {
+                mainView.MainFrame.setBackgroundResource(dayBackgroundColor)
+                mainView.ImageSun.setImageResource(R.drawable.w01d)
+                mainView.ImageSun.animation = sunTopAnim
+            }
+            "09d" -> {
+                mainView.MainFrame.setBackgroundResource(dayBackgroundColor)
+                mainView.ImageSun.setImageResource(R.drawable.w01d)
+                mainView.ImageSun.animation = sunTopAnim
+            }
+            "10d" -> {
+                mainView.MainFrame.setBackgroundResource(dayBackgroundColor)
+                mainView.ImageSun.setImageResource(R.drawable.w01d)
+                mainView.ImageSun.animation = sunTopAnim
+            }
+            "11d" -> {
+                mainView.MainFrame.setBackgroundResource(dayBackgroundColor)
+                mainView.ImageSun.setImageResource(R.drawable.w01d)
+                mainView.ImageSun.animation = sunTopAnim
+            }
+            "13d" -> {
+                mainView.MainFrame.setBackgroundResource(dayBackgroundColor)
+                mainView.ImageSun.setImageResource(R.drawable.w01d)
+                mainView.ImageSun.animation = sunTopAnim
+            }
+            "50d" -> {
+                mainView.MainFrame.setBackgroundResource(dayBackgroundColor)
+                mainView.ImageSun.setImageResource(R.drawable.w01d)
+                mainView.ImageSun.animation = sunTopAnim
+            }
+            "01n" -> {
+                mainView.MainFrame.setBackgroundResource(dayBackgroundColor)
+                mainView.ImageSun.setImageResource(R.drawable.w01d)
+                mainView.ImageSun.animation = sunTopAnim
+                GetClearSkyPhrase(weather.temp.toInt())
+            }
+            "02n" -> {
+                mainView.MainFrame.setBackgroundResource(dayBackgroundColor)
+                mainView.ImageSun.setImageResource(R.drawable.w01d)
+                mainView.ImageSun.animation = sunTopAnim
+            }
+            "03n" -> {
+                mainView.MainFrame.setBackgroundResource(dayBackgroundColor)
+                mainView.ImageSun.setImageResource(R.drawable.w01d)
+                mainView.ImageSun.animation = sunTopAnim
+            }
+            "04n" -> {
+                mainView.MainFrame.setBackgroundResource(dayBackgroundColor)
+                mainView.ImageSun.setImageResource(R.drawable.w01d)
+                mainView.ImageSun.animation = sunTopAnim
+            }
+            "09n" -> {
+                mainView.MainFrame.setBackgroundResource(dayBackgroundColor)
+                mainView.ImageSun.setImageResource(R.drawable.w01d)
+                mainView.ImageSun.animation = sunTopAnim
+            }
+            "10n" -> {
+                mainView.MainFrame.setBackgroundResource(dayBackgroundColor)
+                mainView.ImageSun.setImageResource(R.drawable.w01d)
+                mainView.ImageSun.animation = sunTopAnim
+            }
+            "11n" -> {
+                mainView.MainFrame.setBackgroundResource(dayBackgroundColor)
+                mainView.ImageSun.setImageResource(R.drawable.w01d)
+                mainView.ImageSun.animation = sunTopAnim
+            }
+            "13n" -> {
+                mainView.MainFrame.setBackgroundResource(dayBackgroundColor)
+                mainView.ImageSun.setImageResource(R.drawable.w01d)
+                mainView.ImageSun.animation = sunTopAnim
+            }
+            "50n" -> {
+                mainView.MainFrame.setBackgroundResource(dayBackgroundColor)
+                mainView.ImageSun.setImageResource(R.drawable.w01d)
+                mainView.ImageSun.animation = sunTopAnim
+            }
+        }
         return mainView
+    }
+
+    fun GetClearSkyPhrase(t: Int){
+        if (t > 20){
+            mainView.Phrase.text = phraze[0]
+        }
+        if (t > 0 && t < 21){
+            mainView.Phrase.text = phraze[1]
+        }
+        if (t > -20 && t < 1){
+            mainView.Phrase.text = phraze[3]
+        }
+        if (t > -20 && t < -19){
+            mainView.Phrase.text = phraze[4]
+        }
     }
 }
